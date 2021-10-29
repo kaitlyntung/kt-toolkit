73708a4ba9808f0f00c43dccb4810d73e1b7e35f
Andrew Sedler <arsedler9@gmail.com>
2021-10-29 15:23:11 -0400

    Add pre-commit hooks and update requirements

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # snel-toolkit
-Implements core neural data processing functionality for many SNEL projects.
+Implements core neural data processing functionality for many SNEL projects. Add `snel-toolkit` to your project repo as a submodule and install it in your Python 3.7 project environment via `pip install -e snel-toolkit`.
--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 
 from setuptools import setup, find_packages
 
+# Avoids duplication of requirements
+with open('requirements.txt') as file:
+    install_requires = file.read().splitlines()
+
 setup(
     name='snel_toolkit',
-    version='0.1',
+    author='Systems Neural Engineering Lab',
+    version='0.0.0dev',
+    install_requires=install_requires,
     packages=find_packages(),
-    install_requires=[
-        'pandas==1.0.3',
-        'scipy==1.4.1',
-        'numpy==1.18.1',
-        'matplotlib==3.1.3',
-        'scikit-learn==0.22.1',
-        'h5py==2.10.0'
-    ],
-    author="Systems Neural Engineering Lab",
 )
